5e86f8a54adf79e65bfa2be8cf914ccabbbac357
Riccardo Gennaro <75367744+lallo-unitn@users.noreply.github.com>
2022-12-10 11:38:59 +0100

    Added demoEJB

---
+++ demoEJB/src/main/java/it/unitn/disi/web/rg209272/demoejb/EJBClient.java
@@ -0,0 +1,29 @@
+package it.unitn.disi.web.rg209272.demoejb;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class EJBClient {
+    EJBClient() {
+        Properties jndiProperties = new Properties();
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+        Context ctx = null;
+        SimpleBeanIF bean = null;
+        try {
+            ctx = new InitialContext(jndiProperties);
+            System.out.println("before");
+            bean = (SimpleBeanIF) ctx.lookup("ejb:/demoEJB-1.0-SNAPSHOT/SimpleBeanImpl!it.unitn.disi.web.rg209272.demoejb.SimpleBeanIF");
+            System.out.println(bean.getClass().getSimpleName());
+            System.out.println(bean.incrementNumber(5));
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String a[]) {
+        EJBClient client = new EJBClient();
+    }
+}
